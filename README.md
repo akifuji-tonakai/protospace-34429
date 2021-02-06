569125eacaa7949d4eea17178a0f995aefaeeebf
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-06 17:11:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# README
+
+# テーブル設計
+
+## users テーブル
+
+| Column     | Type   | Options     |
+| ---------- | ------ | ----------- |
+| name       | string | null: false |
+| email      | string | null: false |
+| password   | string | null: false |
+| profile    | text   | null: false |
+| occupation | text   | null: false |
+| position   | text   | null: false |
+- has_many :comments
+- has_many :prototypes
+
+
+## prototypes テーブル
+
+| Column     | Type      | Options                       |
+| ---------- | --------- | ----------------------------- |
+| title      | string    | null: false                   |
+| catch_copy | text      | null: false                   |
+| concept    | text      | null: false                   |
+| user       | reference | null:false, foreign_key: true |
+- has_many :comments
+- belongs_to :user
+
+
+## comments テーブル
+
+| Column    | Type       | Options                        |
+| --------- | ---------- | ------------------------------ |
+| text      | string     |                                |
+| user      | references | null: false, foreign_key: true |
+| prototype | references | null: false, foreign_key: true |
+- belongs_to user
+- belongs_to prototypes
---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,59 @@
+class PrototypesController < ApplicationController
+before_action :prototype_find_id, only: [:edit, :show, :update]
+before_action :authenticate_user!, only: [:new, :edit, :destroy]
+before_action :move_to_root, only: :edit
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    prototype = Prototype.new(prototype_params)
+    if prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.all
+  end
+
+  private
+  def prototype_find_id
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_root
+    unless user_signed_in? && current_user == @prototype.user
+      redirect_to action: :index
+    end
+  end
+end
